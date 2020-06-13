3c05331a774b34b1fd88b2f67174a2a05b90dab3
Mubashshir <ahmubashshir@gmail.com>
2020-06-13 19:58:17 +0600

    fixed a few bugs.

--- danime/test/00-load.bash
+++ danime/test/00-load.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bats
 load 'support/load'
 load 'assert/load'
+source ../.env_setup
 assert_dep_py3()
 {
 	run bash -c "python3 -c 'import $1' &>/dev/null||(echo python3 module $1 not found. && return 1)"
